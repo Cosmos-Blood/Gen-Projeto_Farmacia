4d1f06e429e2d96de92709fa9e144b3fb1d43eec
MacaulyFragoso <macaulyaparecido@gmail.com>
2021-10-02 12:07:06 -0300

    [Feature Macauly - ADD Produto Class & Produto Interface | 02.10.21]

--- farmacia/src/main/java/com/gen/farmacia/repository/CategoriaRepository.java
+++ farmacia/src/main/java/com/gen/farmacia/repository/CategoriaRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gen.farmacia.model.CategoriaModel;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
 
 	public Optional<CategoriaModel> findById(Long id);
