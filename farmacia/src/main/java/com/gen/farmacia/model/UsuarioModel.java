6d1677de2627394b532612e3743a69218ede723d
Zoraten <cosmos_blood@outlook.com>
2021-10-14 18:31:48 -0300

    [Feature Guilherme: - Add UsuarioModel | 14.10.21]

---
+++ farmacia/src/main/java/com/gen/farmacia/model/UsuarioModel.java
@@ -0,0 +1,49 @@
+package com.gen.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class UsuarioModel {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @Id Long idUsuario;
+	
+	private @NotBlank @Email String emailUsuario;
+	
+	@Size(min = 7, max = 75)
+	private @NotBlank String senhaUsuario;
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+	
+	
+}
